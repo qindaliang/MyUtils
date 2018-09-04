0e5bb9fbc8da24a72a735d286a052d3de727674f
solexrd106 <1325716331@qq.com>
2018-09-04 10:14:43 +0800

    ss

---
+++ app/src/main/java/com/qin/myutils/retrofit/common/ErrorCode.java
@@ -0,0 +1,7 @@
+
+package com.qin.myutils.retrofit.common;
+
+public class ErrorCode {
+    public static final int TOKEN_NOT_EXIST = 1000;
+    public static final int TOKEN_INVALID = 1001;
+}
---
+++ app/src/main/java/com/qin/myutils/retrofit/common/RetrofitUtils.java
@@ -0,0 +1,47 @@
+package com.qin.myutils.retrofit.common;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.qin.myutils.retrofit.interceptor.HttpCacheInterceptor;
+import com.qin.myutils.retrofit.interceptor.HttpHeaderInterceptor;
+import com.qin.myutils.retrofit.interceptor.LoggingInterceptor;
+import com.qin.myutils.utils.ContextUtils;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ *
+ */
+
+public class RetrofitUtils {
+    public static OkHttpClient.Builder getOkHttpClientBuilder() {
+        File cacheFile = new File(ContextUtils.getContext().getCacheDir(), "cache");
+        Cache cache = new Cache(cacheFile, 1024 * 1024 * 100); //100Mb
+
+        return new OkHttpClient.Builder()
+                .readTimeout(Constants.DEFAULT_TIMEOUT, TimeUnit.MILLISECONDS)
+                .connectTimeout(Constants.DEFAULT_TIMEOUT, TimeUnit.MILLISECONDS)
+                .addInterceptor(new LoggingInterceptor())
+                .addInterceptor(new HttpHeaderInterceptor())
+                .addNetworkInterceptor(new HttpCacheInterceptor())
+                // .sslSocketFactory(SslContextFactory.getSSLSocketFactoryForTwoWay())
+                // https认证 如果要使用https且为自定义证书 可以去掉这两行注释，并自行配制证书。
+                // .hostnameVerifier(new SafeHostnameVerifier())
+                .cache(cache);
+    }
+
+    public static Retrofit.Builder getRetrofitBuilder(String baseUrl) {
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").serializeNulls().create();
+        OkHttpClient okHttpClient = RetrofitUtils.getOkHttpClientBuilder().build();
+        return new Retrofit.Builder()
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .baseUrl(baseUrl);
+    }
+}
